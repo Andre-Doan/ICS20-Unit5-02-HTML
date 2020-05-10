8925f41aec3f6fd50293c67cf56105108226ba69
Ubuntu <ubuntu@ip-172-31-85-222.ec2.internal>
2020-05-10 21:23:32 +0000

    added index.html file

--- js.js
+++ js.js
@@ -2,14 +2,12 @@
   let x = document.getElementById("numbersubmit").value;
   if(x == 0)
    {
-       alert("This number is neutral"); 
+       alert("This number is neutral");
    }
-   
    else if(x > 0)
    {
-       alert("this number is positive "); 
+       alert("this number is positive ");
    }
-   
    else
    {
        alert("This number is negative");
