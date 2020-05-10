a952419aee773bbd7952c6873e34de00c48f44d9
Ubuntu <ubuntu@ip-172-31-85-222.ec2.internal>
2020-05-10 21:19:16 +0000

    added index.html

---
+++ js.js
@@ -0,0 +1,17 @@
+  document.getElementById("guess").onclick = function(){
+  let x = document.getElementById("numbersubmit").value;
+  if(x == 0)
+   {
+       alert("This number is neutral"); 
+   }
+   
+   else if(x > 0)
+   {
+       alert("this number is positive "); 
+   }
+   
+   else
+   {
+       alert("This number is negative");
+   }
+};
